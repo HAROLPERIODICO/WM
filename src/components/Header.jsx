b15f150e22b16907e9dbfae240ecf4e929ce3b46
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-03 16:24:54 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       {/* Contenedor fijo para logos CHM */}
-      <div className="fixed top-0 left-0 w-full flex justify-between items-center p-2 bg-white z-20">
+      <div className="fixed top-0 left-0 w-full flex justify-between items-center p-2 bg-blue z-20">
         <img src="/logochm.png" alt="Logo CHM" className="h-10 object-contain" loading="lazy" />
         <img src="/banderachm.png" alt="Bandera CHM" className="h-10 object-contain" loading="lazy" />
       </div>
