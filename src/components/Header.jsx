ace9272531874ff5dbd73a9fd124ff6823a9d660
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-03 14:36:37 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       {/* Logos CHM */}
-      <div className="w-full flex justify-between items-center p-2 bg-white" /> 
+      <div className="w-full flex justify-between items-center p-2 bg-white">
         <img src="/logochm.png" alt="Logo CHM" className="h-10 object-contain" loading="lazy" />
         <img src="/banderachm.png" alt="Bandera CHM" className="h-10 object-contain" loading="lazy" />
       </div>
@@ -28,7 +28,7 @@ const Header = () => {
             id="menu-toggle"
             aria-label="Abrir menú"
           >
-            &
+            &#9776;
           </button>
 
           {/* Navegación */}
