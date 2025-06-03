90e8f05115c09abc2047bcb7b22dd63d2db0f9e4
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-03 17:55:26 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,13 +15,13 @@ const Header = () => {
           {/* Logo y título */}
           <h1 className="flex items-center gap-2 text-lg font-semibold">
             <a href="#" className="flex items-center gap-2">
-              <img src="/logo75.png" alt="Logo MARC PALAS" className="h-10" loading="lazy" />
+              <img src="/logo75.png" alt="Logo MARC PALAS" className="h-15" loading="lazy" />
               Work Management
               PALAS Cerrejón
             </a>
           </h1>
-        <img src="/logochm.png" alt="Logo CHM" className="fixed top-2 h-2 object-contain" loading="lazy" />
-        <img src="/banderachm.png" alt="Bandera CHM" className="fixed right-2 top-2 h-10 object-contain" loading="lazy" />
+        <img src="/logochm.png" alt="Logo CHM" className="fixed left-2 top-2 h-20 object-contain" loading="lazy" />
+        <img src="/banderachm.png" alt="Bandera CHM" className="fixed right-2 top-2 h-20 object-contain" loading="lazy" />
           {/* Menú hamburguesa */}
           <button
             className="text-2xl md:hidden"
@@ -34,7 +34,7 @@ const Header = () => {
           {/* Navegación */}
           <nav className="hidden md:flex space-x-6" id="nav">
             <ul className="flex space-x-4" id="menu">
-              <li><a href="#indicadores" className="hover:text-yellow-400 text-underline-decoration-sky-500">Indicadores</a></li>
+              <li><a href="#indicadores" className="hover:text-yellow-400 hover:text-underline-decoration-sky-500">Indicadores</a></li>
               <li><a href="#nosotros" className="hover:text-yellow-400">Nosotros</a></li>
               <li><a href="#status" className="hover:text-yellow-400">Estatus</a></li>
               <li><a href="#programacion" className="hover:text-yellow-400">Programación</a></li>
