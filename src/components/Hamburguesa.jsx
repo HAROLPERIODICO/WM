14fb42dc4d630f4da10545608d5255cea16242a7
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-04 11:53:42 -0500

    Update Hamburguesa.jsx

--- src/components/Hamburguesa.jsx
+++ src/components/Hamburguesa.jsx
@@ -8,59 +8,59 @@ const Header = () => {
   };
 
   return (
-    
-           <button
-            className="text-2xl md:hidden"
-            id="menu-toggle"
-            aria-label="Abrir menú"
-          >
-            &#9776;
-          </button>
-
-                 
-          <nav
-              className={${menuAbierto ? "block" : "hidden"} md:block space-x-6}
-              id="nav"
-            >
-            <ul className="flex flex-col md:flex-row space-x-4" id="menu">
-              <li>
-                <a href="                                                
-                  Indicadores
-                </a>
-              </li>
-              <li>
-                <a href="#nosotros" className="hover:text-yellow-400">
-                  Nosotros
-                </a>
-              </li>
-              <li>
-                <a href="                                           
-                  Estatus
-                </a>
-              </li>
-              <li>
-                <a href="#programacion" className="hover:text-yellow-400">
-                  Programación
-                </a>
-              </li>
-              <li>
-                <a href="                                       
-                  RE
-                </a>
-              </li>
-              <li>
-                <a href="#backlog" className="hover:text-yellow-400">
-                  BACKLOG
-                </a>
-              </li>
-              <li>
-                <a href="#inspecciones" className="hover:text-yellow-400">
-                  Inspecciones
-                </a>
-              </li>
-            </ul>
-          </nav>
-        );
-      };
+    <div>
+      <button
+        className="text-2xl md:hidden"
+        id="menu-toggle"
+        aria-label="Abrir menú"
+        onClick={toggleMenu}
+      >
+        &#9776;
+      </button>
+      <nav
+        className={${menuAbierto ? "block" : "hidden"} md:block space-x-6}
+        id="nav"
+      >
+        <ul className="flex flex-col md:flex-row space-x-4" id="menu">
+          <li>
+            <a href="                                                
+              Indicadores
+            </a>
+          </li>
+          <li>
+            <a href="#nosotros" className="hover:text-yellow-400">
+              Nosotros
+            </a>
+          </li>
+          <li>
+            <a href="                                            
+              Estatus
+            </a>
+          </li>
+          <li>
+            <a href="#programacion" className="hover:text-yellow-400">
+              Programación
+            </a>
+          </li>
+          <li>
+            <a href="                                       
+              RE
+            </a>
+          </li>
+          <li>
+            <a href="#backlog" className="hover:text-yellow-400">
+              BACKLOG
+            </a>
+          </li>
+          <li>
+            <a href="#inspecciones" className="hover:text-yellow-400">
+              Inspecciones
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
