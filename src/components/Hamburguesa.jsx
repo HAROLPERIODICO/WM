eaddc874979cce1d1bdff956bbd46ace69c52eaa
HD-SOFTWARE-SOLUTIONS <haroldiazmelendez@gmail.com>
2025-06-04 11:39:03 -0500

    Update Hamburguesa.jsx

--- src/components/Hamburguesa.jsx
+++ src/components/Hamburguesa.jsx
@@ -18,51 +18,51 @@ const Header = () => {
           </button>
 
      
-    <nav
-      className={`${
-        menuAbierto ? "block" : "hidden"
-      } md:flex space-x-6`}
-      id="nav"
-    >
-      <ul className="flex flex-col md:flex-row space-x-4" id="menu">
-        <li>
-          <a href="                                                
-            Indicadores
-          </a>
-        </li>
-        <li>
-          <a href="#nosotros" className="hover:text-yellow-400">
-            Nosotros
-          </a>
-        </li>
-        <li>
-          <a href="                                           
-            Estatus
-          </a>
-        </li>
-        <li>
-          <a href="#programacion" className="hover:text-yellow-400">
-            Programación
-          </a>
-        </li>
-        <li>
-          <a href="                                       
-            RE
-          </a>
-        </li>
-        <li>
-          <a href="#backlog" className="hover:text-yellow-400">
-            BACKLOG
-          </a>
-        </li>
-        <li>
-          <a href="#inspecciones" className="hover:text-yellow-400">
-            Inspecciones
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+          <nav
+            className={`${
+              menuAbierto ? "block" : "hidden"
+            } md:flex space-x-6`}
+            id="nav"
+          >
+            <ul className="flex flex-col md:flex-row space-x-4" id="menu">
+              <li>
+                <a href="                                                
+                  Indicadores
+                </a>
+              </li>
+              <li>
+                <a href="#nosotros" className="hover:text-yellow-400">
+                  Nosotros
+                </a>
+              </li>
+              <li>
+                <a href="                                           
+                  Estatus
+                </a>
+              </li>
+              <li>
+                <a href="#programacion" className="hover:text-yellow-400">
+                  Programación
+                </a>
+              </li>
+              <li>
+                <a href="                                       
+                  RE
+                </a>
+              </li>
+              <li>
+                <a href="#backlog" className="hover:text-yellow-400">
+                  BACKLOG
+                </a>
+              </li>
+              <li>
+                <a href="#inspecciones" className="hover:text-yellow-400">
+                  Inspecciones
+                </a>
+              </li>
+            </ul>
+          </nav>
+        );
+      };
 
 export default Header;
